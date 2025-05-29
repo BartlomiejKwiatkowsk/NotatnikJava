4098537af506ef378982986ee5a1cdbf04ac5dbe
BartlomiejKwiatkowsk <163582621+BartlomiejKwiatkowsk@users.noreply.github.com>
2025-05-29 18:21:09 +0200

    Update V1__init.sql
    dodanie ról i użytkowników do migracji V1_init.sql dla inicjalizacji bazy danych

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -16,3 +16,11 @@ CREATE TABLE IF NOT EXISTS note (
                                     created_at TIMESTAMP DEFAULT now(),
     user_id BIGINT REFERENCES app_user(id) ON DELETE CASCADE
     );
+INSERT INTO role (name) VALUES ('USER') ON CONFLICT (name) DO NOTHING;
+INSERT INTO role (name) VALUES ('ADMIN') ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO app_user (username, password, role_id)
+VALUES ('user', 'userpass', (SELECT id FROM role WHERE name = 'USER')) ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO app_user (username, password, role_id)
+VALUES ('admin', 'adminpass', (SELECT id FROM role WHERE name = 'ADMIN')) ON CONFLICT (username) DO NOTHING;
